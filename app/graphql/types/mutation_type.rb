6a4ee3bc8cb4b1b4a565671e2cd146bdc38926a2
adam-osu <kasho.dev@gmail.com>
2020-07-26 14:45:53 -0400

    Add add item and update item mutations

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :sign_in, mutation: Mutations::SignInMutation
+    field :update_item, mutation: Mutations:UpdateItemMutation
   end
 end
--- app/javascript/components/Library/index.js
+++ app/javascript/components/Library/index.js
@@ -1,31 +1,8 @@
-import React from "react";
-import { Query } from "react-apollo";
-import gql from "graphql-tag";
+import React from 'react';
+import cs from './styles';
 
-const LibraryQuery = gql`
-  {
-    items {
-      id
-      title
-      user {
-        email
-      }
-    }
-  }
-`;
-
-export default () => (
-  <Query query={LibraryQuery}>
-    {({ data, loading }) => (
-      <div>
-        {loading
-          ? "Loading..."
-          : data.items.map(({ title, id, user }) => (
-              <div key={id}>
-                <b>{title}</b> {user ? `added by ${user.email}` : null}
-              </div>
-            ))}
-      </div>
-    )}
-  </Query>
+const Library = () => (
+  <div className={cs.library}>👋</div>
 );
+
+export default Library;
